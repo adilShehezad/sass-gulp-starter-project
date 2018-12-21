07351033a6d588bd6929fe62f1dd9fcd6e25c5de
adildad2@gmail.com <adildad2@gmail.com>
2018-12-20 17:55:10 -0800

    All required files and basic src directory

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # sass-gulp-starter-project
-This basic sass gulp starter project.  You can use use this in your projects. I will keep updating this file. 
+
+
+This project is for biggners who are new to use gulp and it's   specifically to html/sass project, no extra dependieces. I've commented the each task in very simple words so it's very easy to understand for bigginers how to change the paths and options.
+
+## Usage
+
+
+### 1. Clone repo
+
+```
+git clone https://github.com/adilShehezad/sass-gulp-starter-project.git
+```
+
+### 2. Go inside cloned repo
+```
+cd sass-gulp-starter-project
+```
+
+
+### 3. Install all dependencies (make sure nodejs with npm is installed on your machine)
+```
+npm install
+```
+
+### 4. Run default gulp task (will open browser window with live reload)
+```
+gulp
+```
+
+### 4. Where to place your code
+
+    Place your html,scss and js files in src folder. All of your files will be compiled  minified and inject to dist folder. 
+
+
+## List of npm packages used
+
+    -gulp
+    -gulp-sass
+    -gulp-minify-html
+    -browser-sync
+    -gulp-concat
+    -gulp-imagemin
+    -gulp-minify-css
+    -gulp-autoprefixer
+    -gulp-rename
+    -gulp-plumber
+    -gulp-size
+    -gulp-sass-lint
+    -gulp-jshint
+    -gulp-uglify
+    -gulp-gh-pages
+    -imagemin-pngquant
+    -jshint
+    -gulp-notify
+    -gulp-cached
+
+
+I'm very thankful to developers of these packages. 
---
+++ gulpfile.js
@@ -0,0 +1,135 @@
+
+//Gulp packages
+var gulp = require('gulp'),
+    gulp_sass = require('gulp-sass'),
+    cssmin = require('gulp-minify-css'),
+    scsslint = require('gulp-sass-lint'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    prefix = require('gulp-autoprefixer'),
+    jshint = require('gulp-jshint'),
+    minifyHTML = require('gulp-minify-html'),
+    gulp_size = require('gulp-size'),
+    gulp_imagemin = require('gulp-imagemin'),
+    imagemin_pngquant = require('imagemin-pngquant'),
+    plumber = require('gulp-plumber'),
+    gulp_hPages = require('gulp-gh-pages'),
+    notify = require('gulp-notify'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload,
+    cache = require('gulp-cached');
+
+//Directory paths
+var path = {
+    rootDest: 'dist/',
+    htmlSrc: './src/*.html',
+    htmlDest: 'dist',
+    scssSrc: './src/scss/**/*.scss',
+    scssDist: 'dist/css',
+    jsSrc: './src/js/*.js',
+    jstemp: './js',
+    jsDest: 'dist/js',
+    imgSrc: './src/img/*',
+    imgDest: 'dist/img'
+};
+
+//Browser sync live reload
+gulp.task('browser-sync', function () {
+    browserSync({
+        server: {
+            baseDir: "dist/"
+        }
+    });
+});
+
+gulp.task('gulp_hPages', function () {
+    return gulp.src('dist/**/*')
+        .pipe(gulp_hPages());
+});
+
+
+//Scss compile, minify, gzip and inject to dist folder
+gulp.task('scss', function () {
+    var onError = function (err) {
+        notify.onError({
+            message: "Error: <%= error.message %>"
+        })(err);
+        this.emit('end');
+    };
+
+    return gulp.src(path.scssSrc)
+        .pipe(plumber({ errorHandler: onError }))
+        .pipe(gulp_sass())
+        .pipe(gulp_size({ gzip: true, showFiles: true }))
+        .pipe(prefix())
+        .pipe(rename('main.css'))
+        .pipe(gulp.dest(path.scssDist))
+        .pipe(reload({ stream: true }))
+
+        .pipe(cssmin())
+        .pipe(gulp_size({ gzip: true, showFiles: true }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(path.scssDist))
+});
+
+//Handle scss errors
+gulp.task('scss-lint', function () {
+    gulp.src(path.scssSrc)
+        .pipe(cache('scsslint'))
+        .pipe(scsslint());
+});
+
+//JS compile, minify, gzip and inject to dist folder
+gulp.task('js', function () {
+    gulp.src(path.jsSrc)
+        .pipe(uglify())
+        .pipe(gulp_size({ gzip: true, showFiles: true }))
+        .pipe(concat(path.jstemp))
+        .pipe(rename({ suffix: '.min.js' }))
+        .pipe(gulp.dest(path.jsDest))
+        .pipe(reload({ stream: true }));
+});
+
+//Handle js errors
+gulp.task('jshint', function () {
+    gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+//Minify html
+gulp.task('minify-html', function () {
+    var opts = {
+        comments: true,
+        spare: true
+    };
+
+    gulp.src('./src/*.html')
+        .pipe(minifyHTML(opts))
+        .pipe(gulp.dest(path.rootDest))
+        .pipe(reload({ stream: true }));
+});
+
+//Minify images and inject to dist folder
+gulp.task('imgmin', function () {
+    return gulp.src(path.imgSrc)
+        .pipe(gulp_imagemin({
+            progressive: true,
+            svgoPlugins: [{ removeViewBox: false }],
+            use: [imagemin_pngquant()]
+        }))
+        .pipe(gulp.dest(path.imgDest));
+});
+
+// Watch for all changes including scss, html, js and images
+gulp.task('watch', function () {
+    gulp.watch('./src/scss/**/*.scss', ['scss']);
+    gulp.watch('./src/js/*.js', ['js']);
+    gulp.watch('./src/*.html', ['minify-html']);
+    gulp.watch('./src/img/*', ['imgmin']);
+});
+
+
+//All the tasks added in default task
+gulp.task('default', ['browser-sync', 'js', 'imgmin', 'minify-html', 'scss', 'watch']);
